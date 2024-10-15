e3ea610dad37c351aa26d4814cdb57027a3018a1
BohdanLytvynov <blitvinov19@gmail.com>
2024-10-16 00:36:22 +0300

    Initial setup

---
+++ DB_Lab7/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace DB_Lab7
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
