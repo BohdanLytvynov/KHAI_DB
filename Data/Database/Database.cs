9b43bb39c20a242278bec81e1ecb90e308d52927
BohdanLytvynov <blitvinov19@gmail.com>
2024-10-19 18:27:16 +0300

    Base Infrastructure added. Work on ViewModel

---
+++ Data/Database/Database.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Database
+{
+    public class Database
+    {
+        #region Properties
+
+        private string m_host;
+        private string m_port;
+        private string m_username;
+        private string m_password;
+        private string m_databaseName;
+
+        #endregion
+
+        #region Ctor
+
+        public Database(string host, string port, string username, string password, string databaseName)
+        {
+            m_host = host;
+            m_port = port;
+            m_username = username;
+            m_password = password;
+            m_databaseName = databaseName;
+        }
+
+        #endregion
+
+        #region Functions
+
+        public IDbConnection Open()
+        { 
+            IDbConnection connection = null;
+
+            return connection;
+        }
+
+        #region Static
+
+        public static IDbCommand BuildCommand()
+        { 
+            
+        }
+
+        #endregion
+
+        #endregion
+    }
+}
---
+++ Data/Realizations/MySqlConnectionBuilder.cs
@@ -0,0 +1,18 @@
+﻿using Data.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Realizations
+{
+    public class MySqlConnectionBuilder : IDbConnectionBuilder
+    {
+        public IDbConnection Buid(string conString)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
